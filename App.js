efe0caa1df558e6c012e9c693f5138a5de1d94a8
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-02-18 07:09:31 -0500

    Add ability to edit ToDo item

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   Alert,
 } from "react-native";
 import { Fontisto } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
 
@@ -29,6 +30,12 @@ export default function App() {
   const travel = () => setListType(false);
   const work = () => setListType(true);
   const onChangeText = (payload) => setText(payload);
+  const handleChangeText = ({ key, text }) => {
+    setToDos({
+      ...toDos,
+      [key]: { ...toDos[key], text },
+    });
+  };
 
   const saveToDos = async (toSave) => {
     try {
@@ -63,6 +70,32 @@ export default function App() {
     setText("");
   };
 
+  const completeToDo = (key) => {
+    toDos[key] = { ...toDos[key], isComplete: !toDos[key].isComplete };
+    const newToDos = { ...toDos, [key]: toDos[key] };
+    setToDos(newToDos);
+    saveToDos(newToDos);
+  };
+
+  const editToDo = (key) => {
+    toDos[key] = { ...toDos[key], isEdit: true };
+    const newToDos = { ...toDos, [key]: toDos[key] };
+    setToDos(newToDos);
+    saveToDos(newToDos);
+  };
+
+  const handleSubmitEditing = async (key) => {
+    if (toDos[key].text === "") return;
+    toDos[key] = { ...toDos[key], isEdit: false };
+
+    const newToDos = {
+      ...toDos,
+      [key]: toDos[key],
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+  };
+
   const deleteToDo = async (key) => {
     Alert.alert("Delete To Do?", "Are you sure?", [
       { text: "Cancel" },
@@ -77,13 +110,6 @@ export default function App() {
         },
       },
     ]);
-  };
-
-  const completeToDo = (key) => {
-    toDos[key] = { ...toDos[key], isComplete: !toDos[key].isComplete };
-    const newToDos = { ...toDos, [key]: toDos[key] };
-    setToDos(newToDos);
-    saveToDos(newToDos);
   };
 
   return (
@@ -127,16 +153,31 @@ export default function App() {
                   <Fontisto name="check" size={18} color={theme.grey} />
                 </Text>
               </TouchableOpacity>
-              <Text
-                style={{
-                  ...styles.toDoText,
-                  textDecorationLine: toDos[key].isComplete
-                    ? "line-through"
-                    : "none",
-                }}
-              >
-                {toDos[key].text}
-              </Text>
+              {toDos[key].isEdit ? (
+                <TextInput
+                  onSubmitEditing={() => handleSubmitEditing(key)}
+                  onChangeText={(text) => handleChangeText({ key, text })}
+                  value={toDos[key].text}
+                  style={styles.inputEdit}
+                  returnKeyType="done"
+                ></TextInput>
+              ) : (
+                <Text
+                  style={{
+                    ...styles.toDoText,
+                    textDecorationLine: toDos[key].isComplete
+                      ? "line-through"
+                      : "none",
+                  }}
+                >
+                  {toDos[key].text}
+                </Text>
+              )}
+              <TouchableOpacity onPress={() => editToDo(key)}>
+                <Text>
+                  <FontAwesome name="pencil" size={18} color={theme.grey} />
+                </Text>
+              </TouchableOpacity>
               <TouchableOpacity onPress={() => deleteToDo(key)}>
                 <Text>
                   <Fontisto name="trash" size={18} color={theme.grey} />
@@ -173,6 +214,15 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginVertical: 20,
     fontSize: 18,
+  },
+  inputEdit: {
+    flex: 0.8,
+    backgroundColor: "white",
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    borderRadius: 5,
+    fontSize: 15,
+    color: theme.grey,
   },
   toDo: {
     backgroundColor: theme.toDoBg,
